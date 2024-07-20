168405ec81365ab70d1b34dfd3a1def0ef217020
Jess-Do <jdo5@students.kennesaw.edu>
2024-07-20 00:24:53 -0400

    Update code.py - corrected the option when user choose "either" and fix some minor typo issues

--- code.py
+++ code.py
@@ -11,7 +11,7 @@ questions = ("How many people are participating?: ",
 options = (("A. One", "B. Two", "C. Three", "D. Four or more" ),
            ("A. 30 minutes", "B. 1-2 hour(s)", "C. Half of the day", "D. The whole day"),
            ("A. Either", "B. Indoors", "C. Outdoors"),
-           ("A. $0 (Free)", "B. $1-$15", "C. $15-$30", "D. $30-$50", "E. $50+"))
+           ("A. $0 (Free)", "B. $1-$15", "C. $15-$30", "D. $30-$50"))
 
 indoors = ['Read a book','Workout','Bake or cook something','Karaoke','Throw a Party','Movie Marathon']
 outdoors = ['Go out for a walk','Go to the movie theater','Go to a spa','Play a sport','Shopping']
@@ -86,17 +86,17 @@ q3_elimination_outdoors = []
 
 while True:
     print("----------------------------------------------------------------------------------------------------------------------------")
-    question_3 = input("What is the budget range?:\n(A) $0(Free)\n(B) $1-$15\n(C) $15-$30\n(D) $30-$50\n(E.) $50+ ")
+    question_3 = input("What is the budget range?:\n(A) $0(Free)\n(B) $1-$15\n(C) $15-$30\n(D) $30-$50 ")
     if question_3 == "A":
         q3_elimination_indoors.append('Throw a Party')
-        q1_elimination_outdoors.append('Go to the movie theater')
-        q1_elimination_outdoors.append('Go to the spa')
-        q1_elimination_outdoors.append('Shopping')
+        q3_elimination_outdoors.append('Go to the movie theater')
+        q3_elimination_outdoors.append('Go to a spa')
+        q3_elimination_outdoors.append('Shopping')
         break
     elif question_3 == "B":
         q3_elimination_indoors.append('Throw a Party')
         q3_elimination_outdoors.append('Go out for a walk')
-        q3_elimination_outdoors.append('Go to the spa')
+        q3_elimination_outdoors.append('Go to a spa')
         break
     elif question_3 == "C":
         q3_elimination_indoors.append('Read a book')
@@ -113,11 +113,21 @@ while True:
         print('[PLEASE PUT A VALID ANSWER]')
 
         
-indoors_total = (q1_elimination_indoors + q2_elimination_indoors + q3_elimination_indoors)
-indoors_final = set(indoors_total)
+indoors_total_e = (q1_elimination_indoors + q2_elimination_indoors + q3_elimination_indoors)
+indoors_minus = set(indoors_total_e)
+indoors_set = set(indoors)
+indoors_final = indoors_set - indoors_minus
 
-outdoors_total = (q1_elimination_outdoors + q2_elimination_outdoors + q3_elimination_outdoors)
-outdoors_final = set(outdoors_total)
+
+outdoors_total_e = (q1_elimination_outdoors + q2_elimination_outdoors + q3_elimination_outdoors)
+outdoors_minus = set(outdoors_total_e)
+outdoors_set = set(outdoors)
+outdoors_final = outdoors_set - indoors_minus
+
+either_total_e = (q1_elimination_indoors + q2_elimination_indoors + q3_elimination_indoors + q1_elimination_outdoors + q2_elimination_outdoors + q3_elimination_outdoors)
+either_minus = set(either_total_e)
+either_set = set(indoors + outdoors)
+either_final = either_set - either_minus
 
 while True:
     print("----------------------------------------------------------------------------------------------------------------------------")
@@ -129,13 +139,18 @@ while True:
         indoors_final.clear()
         break
     elif question_4 == "A":
+        either_final
         break
     else:
         print("----------------------------------------------------------------------------------------------------------------------------")
         print('[PLEASE PUT A VALID ANSWER]')
 
 final = indoors_final.union(outdoors_final)
+final_either = either_final
 print("----------------------------------------------------------------------------------------------------------------------------")
 print('Thank you for answering! Here are your option(s):')
 
-print(final)
+if question_4 == "A":
+    print(final_either)
+else:
+    print(final)
